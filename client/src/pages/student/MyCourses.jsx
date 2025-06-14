95e71e62d558f3d6f3054f311143eb28fcfc177f
swetasachan777 <swetasachan777@gmail.com>
2025-06-15 01:58:44 +0530

    modify app.jsx

--- client/src/pages/student/MyCourses.jsx
+++ client/src/pages/student/MyCourses.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/navbar";
+
 const myCourses = [
   {
     id: 1,
--- client/src/pages/student/StudentProfile.jsx
+++ client/src/pages/student/StudentProfile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/navbar";
+
 const StudentProfile = () => {
   const studentData = {
     name: "Sweta Sharma",
